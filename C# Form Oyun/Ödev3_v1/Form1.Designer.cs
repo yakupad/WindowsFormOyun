b8fc091d07b395ee07d1dc75be5eb7596a8889c0
Yakup AD <yakupad@ogr.cbu.edu.tr>
2016-12-29 01:41:10 +0300

    form oyun

---
+++ C# Form Oyun/Ödev3_v1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Ödev3_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripscore = new System.Windows.Forms.ToolStripTextBox();
+            this.StripNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.bestScoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stripbestscore = new System.Windows.Forms.ToolStripTextBox();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem,
+            this.scoreToolStripMenuItem,
+            this.stripscore,
+            this.StripNew,
+            this.bestScoreToolStripMenuItem,
+            this.stripbestscore,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(975, 31);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(48, 27);
+            this.playToolStripMenuItem.Text = "Play";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // scoreToolStripMenuItem
+            // 
+            this.scoreToolStripMenuItem.Name = "scoreToolStripMenuItem";
+            this.scoreToolStripMenuItem.Size = new System.Drawing.Size(61, 27);
+            this.scoreToolStripMenuItem.Text = "Score:";
+            // 
+            // stripscore
+            // 
+            this.stripscore.Name = "stripscore";
+            this.stripscore.ReadOnly = true;
+            this.stripscore.Size = new System.Drawing.Size(100, 27);
+            // 
+            // StripNew
+            // 
+            this.StripNew.Name = "StripNew";
+            this.StripNew.Size = new System.Drawing.Size(55, 27);
+            this.StripNew.Text = "New ";
+            this.StripNew.Visible = false;
+            this.StripNew.Click += new System.EventHandler(this.StripNew_Click);
+            // 
+            // bestScoreToolStripMenuItem
+            // 
+            this.bestScoreToolStripMenuItem.Name = "bestScoreToolStripMenuItem";
+            this.bestScoreToolStripMenuItem.Size = new System.Drawing.Size(89, 27);
+            this.bestScoreToolStripMenuItem.Text = "BestScore:";
+            // 
+            // stripbestscore
+            // 
+            this.stripbestscore.Name = "stripbestscore";
+            this.stripbestscore.ReadOnly = true;
+            this.stripbestscore.Size = new System.Drawing.Size(100, 27);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(62, 27);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Visible = false;
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(45, 27);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 645);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ödev3";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem scoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox stripscore;
+        private System.Windows.Forms.ToolStripMenuItem StripNew;
+        private System.Windows.Forms.ToolStripMenuItem bestScoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox stripbestscore;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ C# Form Oyun/Ödev3_v1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev3_v1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        Button[,] b = new Button[8, 8];//Çift boyutlu bir button array tanımladık
+        int best=0;
+        private void ekranolustur()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    b[i,j] = new Button();
+                    b[i,j].Size=new Size(50, 50);
+                    b[i, j].Location = new Point(j * 50 + 50, i * 50 + 50);
+                    b[i, j].BackColor = Color.FromKnownColor(KnownColor.Control);
+                    b[i, j].Margin = new Padding(0);
+                    b[i, j].Tag =8*i+j;//Tıkladığımızda o tıklanan butona ulaşa bilmek için her bir butona bir tag degeri atadık. 
+                    this.Controls.Add(b[i, j]);//Butonlarımızı form ekranına ekledik.
+                    b[i, j].Click += B_Click;
+                    
+                }
+            }
+
+        }
+        int x, y;
+        int score = 0;
+        private void B_Click(object sender, EventArgs e)
+        {
+            Button tik = (Button)sender;
+            score++;
+            x = (int)tik.Tag / 8;//x ve y degerlerini tiklanan butonun tag degerinden mod ve bölme işlemi ile aldık
+            y = (int)tik.Tag % 8;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (b[i, j].BackColor == Color.Yellow)
+                        b[i, j].BackColor = Color.FromKnownColor(KnownColor.Control);//Gitme ihtimalı sarı olan buton reklerini default haline döndür.
+                }
+            }
+            //en iyi ihtimalle 8 olasılık olucagı için 8 ihtimalın gerçekleşme ihtimallerini belirledik ve gidebileceği yolları sarı renk olarak belirledik.
+                        if ((7 - y > 1) && (x > 0))
+                        {
+                            if(b[x - 1, y + 2].BackColor != Color.Red)// Eger önceden gidilmediyse backcolor sarı olucak. 
+                               b[x - 1, y + 2].BackColor = Color.Yellow;
+                        }
+
+                        if ((7 - y > 1) && (7 - x > 0))
+                        {
+                            if (b[x + 1, y + 2].BackColor != Color.Red)
+                                b[x + 1, y + 2].BackColor = Color.Yellow;
+                        }
+                        if ((y > 1) && (x > 0))
+                        {
+                            if (b[x - 1, y - 2].BackColor != Color.Red)
+                                b[x - 1, y - 2].BackColor = Color.Yellow;
+                        }
+                        if ((y > 1) && (7 - x > 0))
+                        {
+                            if (b[x + 1, y - 2].BackColor != Color.Red)
+                                b[x + 1, y - 2].BackColor = Color.Yellow;
+                        }
+
+                        if ((x > 1) && (y > 0))
+                        {
+                            if (b[x - 2, y - 1].BackColor != Color.Red)
+                                b[x - 2, y - 1].BackColor = Color.Yellow;
+                        }
+                        if ((x > 1) && (7 - y > 0))
+                        {
+                            if (b[x - 2, y + 1].BackColor != Color.Red)
+                                b[x - 2, y + 1].BackColor = Color.Yellow;
+                        }
+                        if ((7 - x > 1) && (y > 0))
+                        {
+                            if (b[x + 2, y - 1].BackColor != Color.Red)
+                                b[x + 2, y - 1].BackColor = Color.Yellow;
+                        }
+                        if ((7 - x > 1) && (7 - y > 0))
+                        {
+                            if (b[x + 2, y + 1].BackColor != Color.Red)
+                                b[x + 2, y + 1].BackColor = Color.Yellow;
+                        }
+                    tik.BackColor = Color.Red;
+                    tik.Text = score.ToString();//her adımda artan score degerini textbox ve button textinde gösterdik.  
+                    stripscore.Text = score.ToString();
+                    if(best<score)
+                    {
+                        best = score;//eger yapılan score bestscoredan iyiyse bestscoru o score yaptık.
+                    }
+                    if (score == 64)
+                            MessageBox.Show("Oyunu Kazandın!!!", "Tebrikler");//kazanma durumundaki mesajımız.
+                    ihtimal();
+                    if(Bittimi())
+                       {
+                            MessageBox.Show("Oyun Bitti Score: " + score.ToString(), "Malesef");//Gidecek yer kalmadığında aldığımız mesaj.
+                            stripbestscore.Text = best.ToString();
+                            aboutToolStripMenuItem.Visible = true;
+                       }
+        }
+        private void ihtimal()//Buttona tıkladığımızda sadece sarı yanan yerlere gidebileceğini belirledik.
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (b[i, j].BackColor == Color.Yellow)
+                    {
+                        b[i, j].Enabled = true;
+                    }
+                    else
+                    {
+                        b[i, j].Enabled = false;
+                    }
+                    if (b[i, j].BackColor == Color.Red)
+                    {
+                        b[i, j].Enabled = false;
+                    }}}
+      }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ekranolustur();
+            StripNew.Visible = true;
+            playToolStripMenuItem.Visible = false;
+        }
+
+        private void StripNew_Click(object sender, EventArgs e)//Yeniden başlamak istenildiğinde Button dizisini boşaltıp tekrardan ekranolustur metodunu çalıştırdık.
+        {
+            stripscore.Text = "0";
+            score = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    this.Controls.Remove(b[i, j]);
+                }
+            }
+            ekranolustur();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Erdem Özgedik ve Fatih Çelik tarafından ödev amacılığıyla gerçekleştirilmiştir.", "Proje Hakkında");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private bool Bittimi()//Gidilecek yol kalıp kalmadığını konrol eden metodumuz. 
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (b[i,j].Enabled==true)
+                    {
+                        return false;
+                    }}}
+            return true;
+        }
+    }
+}
